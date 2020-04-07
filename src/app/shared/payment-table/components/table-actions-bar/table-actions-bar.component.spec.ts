dd17a26cbab8f559c523be8bbae2107c92361d17
alexpidkuiko <o.pidkuiko@andersenlab.com>
2020-04-07 18:46:27 +0300

    Added tests for table-actions-bar.component

--- src/app/shared/payment-table/components/table-actions-bar/table-actions-bar.component.spec.ts
+++ src/app/shared/payment-table/components/table-actions-bar/table-actions-bar.component.spec.ts
@@ -1,6 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync } from '@angular/core/testing';
 
 import { TableActionsBarComponent } from './table-actions-bar.component';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { IPaymentTableRow } from '../../../../utils/interfaces/payment-table-interfaces';
+import {
+  INVALID_MOCK_PAYMENT_NAME, INVALID_MOCK_PRICE_BY_DAY,
+  MOCK_ROW_DATA,
+  VALID_MOCK_PAYMENT_NAME,
+  VALID_MOCK_PRICE_BY_DAY
+} from '../../../../utils/mock/payment-table-mock-data';
 
 describe('TableActionsBarComponent', () => {
   let component: TableActionsBarComponent;
@@ -8,7 +17,17 @@ describe('TableActionsBarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TableActionsBarComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        TableActionsBarComponent
+      ],
+      providers: [
+        FormBuilder
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
@@ -19,7 +38,39 @@ describe('TableActionsBarComponent', () => {
     fixture.detectChanges();
   });
 
+  function updateForm(paymentName, priceByDay) {
+    component.paymentFormGroup.controls['paymentName'].setValue(paymentName);
+    component.paymentFormGroup.controls['priceByDay'].setValue(priceByDay);
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should emit addPaymentData after emitAddRow call', () => {
+    let newPaymentData: IPaymentTableRow;
+    component.addPaymentData.subscribe((data) => newPaymentData = data );
+
+    component.emitAddRow();
+    expect(newPaymentData).toBeDefined();
+  });
+
+  it('paymentFormGroup should be Defined after initForm call. initForm called in ngOnInit)', () => {
+    expect(component.paymentFormGroup).toBeDefined();
+  });
+
+  it('form value should update from form changes', fakeAsync(() => {
+    updateForm(VALID_MOCK_PAYMENT_NAME, VALID_MOCK_PRICE_BY_DAY);
+    expect(component.paymentFormGroup.value).toEqual(MOCK_ROW_DATA);
+  }));
+
+  it('form status should be valid for valid data', fakeAsync(() => {
+    updateForm(VALID_MOCK_PAYMENT_NAME, VALID_MOCK_PRICE_BY_DAY);
+    expect(component.paymentFormGroup.valid).toBeTruthy();
+  }));
+
+  it('form status should be invalid for invalid data', fakeAsync(() => {
+    updateForm(INVALID_MOCK_PAYMENT_NAME, INVALID_MOCK_PRICE_BY_DAY);
+    expect(component.paymentFormGroup.invalid).toBeTruthy();
+  }));
 });
--- src/app/utils/mock/payment-table-mock-data.ts
+++ src/app/utils/mock/payment-table-mock-data.ts
@@ -10,4 +10,12 @@ export const TABLE_ROW_DATA: IPaymentTableRow[] = [
   {paymentName: 'Подписка на музыку', priceByDay: 15},
 ];
 
-export const MOCK_ROW_DATA: IPaymentTableRow = {paymentName: 'test', priceByDay: 2};
+export const MOCK_ROW_DATA: IPaymentTableRow = {paymentName: 'not empty line', priceByDay: 10};
+
+export const VALID_MOCK_PAYMENT_NAME = 'not empty line';
+
+export const INVALID_MOCK_PAYMENT_NAME = '';
+
+export const VALID_MOCK_PRICE_BY_DAY = 10;
+
+export const INVALID_MOCK_PRICE_BY_DAY = -1;

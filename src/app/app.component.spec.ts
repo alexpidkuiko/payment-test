4a280030715d99efbdf262053aaba36e7884f1ab
alexpidkuiko <o.pidkuiko@andersenlab.com>
2020-04-07 17:46:27 +0300

    Added tests for app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,47 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { PaymentTableModule } from './shared/payment-table/payment-table.module';
+import { PaymentTableService } from './utils/services/payment-table-service';
+import { MOCK_ROW_DATA } from './utils/mock/payment-table-mock-data';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let paymentTableService: PaymentTableService;
+  let spyOfRemoveTableRowMethod: jasmine.Spy;
+  let spyOfAddPaymentDataMethod: jasmine.Spy;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        PaymentTableModule
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'payment-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('payment-app');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    paymentTableService = fixture.debugElement.injector.get(PaymentTableService);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to payment-app!');
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call addRow of paymentTableService', () => {
+    spyOfAddPaymentDataMethod = spyOn(paymentTableService, 'addRow');
+    component.addPaymentData(MOCK_ROW_DATA);
+    expect(spyOfAddPaymentDataMethod.calls.any()).toBeTruthy();
+  });
+
+  it('should call removeRow of paymentTableService', () => {
+    spyOfRemoveTableRowMethod = spyOn(paymentTableService, 'removeRow');
+    component.removeTableRow(0);
+    expect(spyOfRemoveTableRowMethod.calls.any()).toBeTruthy();
   });
 });
